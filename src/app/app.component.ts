1c4ff25950a00ea10f14b2c83341a5800502c8fd
NghiNV <nghinv03@gmail.com>
2024-09-10 23:27:00 +0700

    Add Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {HeaderComponent} from "./app/header.component";
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+bootstrapApplication(HeaderComponent);
