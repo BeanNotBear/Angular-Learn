5937f93c8a3dee33b0987699938e5ce7ed412371
NghiNV <nghinv03@gmail.com>
2024-09-12 22:23:38 +0700

    Working with undefine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import {UserComponent} from "./user/user.component";
+import {DUMMY_USERS} from "./user/dummy-users";
+import {TaskComponent} from "./task/task.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-app-angular';
+  users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,5 @@
-import {Component, computed, signal} from '@angular/core';
+import {Component, computed, EventEmitter, Input, Output} from '@angular/core';
 import { DUMMY_USERS } from "./dummy-users";
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,16 +9,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex])
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
-  avatarPath = computed(() => "assets/users/" + this.selectedUser().avatar)
+  @Output() select = new EventEmitter<string>();
 
-  // get userImg() {
-  //   return "assets/users/" + this.selectedUser().avatar;
-  // }
+  get avatarPath() {
+    return "assets/users/" + this.avatar;
+  }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-  }
+    this.select.emit(this.id);
+  };
 }
