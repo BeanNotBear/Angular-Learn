7240164b7c22911dd63b8c8c92a82aac13a25598
NghiNV <nghinv03@gmail.com>
2024-09-12 15:24:11 +0700

    Display random user

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {HeaderComponent} from "./app/header.component";
+import {HeaderComponent} from "./app/header/header.component";
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
