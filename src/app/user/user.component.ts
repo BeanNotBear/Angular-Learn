85a8a19821a5206366009f272c3b4d94b95cd032
NghiNV <nghinv03@gmail.com>
2024-09-12 16:08:13 +0700

    Introduction to Signal

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import {Component, computed, signal} from '@angular/core';
+import { DUMMY_USERS } from "./dummy-users";
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  selectedUser = signal(DUMMY_USERS[randomIndex])
+
+  avatarPath = computed(() => "assets/users/" + this.selectedUser().avatar)
+
+  // get userImg() {
+  //   return "assets/users/" + this.selectedUser().avatar;
+  // }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
+}
