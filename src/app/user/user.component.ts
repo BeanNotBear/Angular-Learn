b453b7b3876eb53a0e4785d7de842fe76117de90
NghiNV <nghinv03@gmail.com>
2024-09-13 11:21:32 +0700

    Dynamic CSS styling with class binding

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,7 +11,7 @@ import {User} from "./user.model";
 })
 export class UserComponent {
   @Input({required: true}) user!: User;
-
+  @Input({required: true}) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get avatarPath() {
